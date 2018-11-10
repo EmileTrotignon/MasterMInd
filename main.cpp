a4bea3bc3bd30aecf32f8d8e9b90a096fedb4c7f
Emile Trotignon <emile.trotignon@laposte.net>
2018-11-10 13:02:11 +0100

    Fixed memory management using unique_ptr

--- main.cpp
+++ main.cpp
@@ -3,34 +3,42 @@
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
+#include <memory>
+
+#define N 2000
 
 using namespace std;
 
+template class std::unique_ptr<int[]>;
+
 template <typename T>
-void copy_array(const T *in, T *out, int t){
-    for (int i=0; i<t; i++){
+void copy_array(const unique_ptr<T> &in, unique_ptr<T> &out, int t) {
+    for (int i = 0; i < t; i++) {
         out[i] = in[i];
     }
 }
 
-void output_tabb(ostream& s, const int *tab, int t){
+void initialize(unique_ptr<int[]> &tab, int t) {
+    for (int i = 0; i < t; i++) tab[t] = 0;
+}
+
+void initialize(unique_ptr<int[]> &tab, int t, const int *ptrr) {
+    for (int i = 0; i < t; i++) tab[t] = ptrr[t];
+}
+
+
+void output_tabb(ostream& s, const unique_ptr<int[]> &tab, int t){
     for (int i=0; i<t; i++) s << tab[i] << " ";
 }
 
-bool in_array(int e, const int *tab, int t){
+bool in_array(int e, const unique_ptr<int[]> &tab, int t){
     for (int i=0; i < t; i++){
         if (e == tab[i]) return true;
     }
     return false;
 }
 
-int index(int e, const int *tab, int t){
-    for (int i=0; i < t; i++){
-        if (e == tab[i]) return i;
-    }
-}
-
-bool in_array_ordered(const int *t1, const int *t2, int i, int t) {
+bool in_array_ordered(const unique_ptr<int[]> &t1, const unique_ptr<int[]> &t2, int i, int t) {
     int e = t1[i];
     int c1 = 1;
     int c2 = 0;
@@ -44,47 +52,105 @@ bool in_array_ordered(const int *t1, const int *t2, int i, int t) {
 
 class MasterMind{
 
-    const int *solution;
+    unique_ptr<int[]>             solution;
 
 public:
 
-    int n_couleurs;
-    int n_pins;
-    int n_coups_max;
-    int n_coups;
-    int *tab_noirs;
-    int *tab_blancs;
-    int **tab_coups;
+    unsigned int                  n_couleurs;
+    unsigned int                  n_pins;
+    unsigned int                  n_coups_max;
+    unsigned int                  n_coups;
+    unique_ptr<int[]>             tab_noirs;
+    unique_ptr<int[]>             tab_blancs;
+    unique_ptr<unique_ptr<int[]>[]> tab_coups;
 
-    MasterMind(const int *sol, int nc, int np, int cmax){
-        solution = sol;
+    MasterMind(unique_ptr<int[]> &sol, unsigned int nc, unsigned int np, unsigned int cmax){
         n_couleurs = nc;
         n_pins = np;
         n_coups_max = cmax;
         n_coups = 0;
-        tab_blancs = new int[cmax];
-        tab_noirs = new int[cmax];
-        tab_coups = new int *[cmax];
+        auto uptr = make_unique<int[]>(np);
+        solution.swap(uptr);
+        copy_array(sol, solution, np);
+
+        auto tb = make_unique<int[]>(cmax);
+        auto tn = make_unique<int[]>(cmax);
+        auto tc = make_unique<unique_ptr<int[]>[]>(cmax);
+
+        tab_blancs.swap(tb);
+        tab_noirs.swap(tn);
+        tab_coups.swap(tc);
+
     }
 
-    MasterMind(int nc, int np, int cmax){
-        auto sol = new int[np];
-        for (int p=0; p<np; p++) sol[p] = rand() % nc;
-        solution = sol;
+    MasterMind(unsigned int nc, unsigned int np, unsigned int cmax){
         n_couleurs = nc;
         n_pins = np;
         n_coups_max = cmax;
         n_coups = 0;
-        tab_blancs = new int[cmax];
-        tab_noirs = new int[cmax];
-        tab_coups = new int *[cmax];
+        auto uptr = make_unique<int[]>(np);
+        solution.swap(uptr);
+        for (int p=0; p<np; p++) solution[p] = rand() % nc;
+
+        auto tb = make_unique<int[]>(cmax);
+        auto tn = make_unique<int[]>(cmax);
+        auto tc = make_unique<unique_ptr<int[]>[]>(cmax);
+
+        tab_blancs.swap(tb);
+        tab_noirs.swap(tn);
+        tab_coups.swap(tc);
+    }
+
+    MasterMind(MasterMind &g){
+        n_couleurs = g.n_couleurs;
+        n_pins = g.n_pins;
+        n_coups_max = g.n_coups_max;
+        n_coups = g.n_coups;
+        auto uptr = make_unique<int[]>(n_pins);
+        solution.swap(uptr);
+        copy_array(g.solution, solution, n_pins);
+
+        auto tb = make_unique<int[]>(n_coups_max);
+        auto tn = make_unique<int[]>(n_coups_max);
+        auto tc = make_unique<unique_ptr<int[]>[]>(n_coups_max);
+
+        tab_blancs.swap(tb);
+        tab_noirs.swap(tn);
+        tab_coups.swap(tc);
     }
 
     ~MasterMind(){
-        for (int i=0; i < n_coups; i++) delete[] tab_coups[i];
+        /*for (int i=0; i < n_coups_max; i++) if (tab_coups[i]) delete[] tab_coups[i];
+        if (tab_coups) delete[] tab_coups;
+        tab_coups = nullptr;
+        if (tab_blancs) delete[] tab_blancs;
+        tab_blancs = nullptr;
+        if (tab_noirs) delete[] tab_noirs;
+        tab_noirs = nullptr;
+        if (solution) delete[] solution;
+        solution = nullptr;*/
     }
 
-    friend ostream& operator<<(ostream &s, MasterMind game){
+    MasterMind &operator=(MasterMind const &g){
+        n_couleurs = g.n_couleurs;
+        n_pins = g.n_pins;
+        n_coups_max = g.n_coups_max;
+        n_coups = g.n_coups;
+        auto uptr = make_unique<int[]>(n_pins);
+        solution.swap(uptr);
+        copy_array(g.solution, solution, n_pins);
+
+        auto tb = make_unique<int[]>(n_coups_max);
+        auto tn = make_unique<int[]>(n_coups_max);
+        auto tc = make_unique<unique_ptr<int[]>[]>(n_coups_max);
+
+        tab_blancs.swap(tb);
+        tab_noirs.swap(tn);
+        tab_coups.swap(tc);
+        return *this;
+    }
+
+    friend ostream& operator<<(ostream &s, MasterMind &game){
         s << "MasterMind\n"
           << "    solution : ";
         output_tabb(s, game.solution, game.n_pins);
@@ -97,7 +163,7 @@ public:
         return s;
     }
 
-    bool play(const int *coup, bool log=false) {
+    bool play(const unique_ptr<int[]> &coup, bool log=false) {
         if (n_coups == n_coups_max - 1) return true;
         int noirs = 0;
         int blancs = 0;
@@ -116,15 +182,16 @@ public:
         }
         tab_noirs[n_coups] = noirs;
         tab_blancs[n_coups] = blancs;
-        tab_coups[n_coups] = new int[n_pins];
-        copy_array<int>(coup, tab_coups[n_coups], n_pins);
+
+        auto new_coup = make_unique<int[]>(n_pins);
+        tab_coups[n_coups].swap(new_coup);
+        copy_array(coup, tab_coups[n_coups], n_pins);
         n_coups++;
         return noirs == n_pins;
     }
-
 };
 
-void next_alphanum(int *tab, int base, int t) {
+void next_alphanum(unique_ptr<int[]> &tab, int base, int t) {
     for (int i=0; i < t; i++) {
         if (tab[i] == base - 1) tab[i] = 0;
         else {
@@ -134,7 +201,7 @@ void next_alphanum(int *tab, int base, int t) {
     }
 }
 
-bool check_sol(const MasterMind &game, const int *test_sol) {
+bool check_sol(const MasterMind &game, unique_ptr<int[]> &test_sol) {
     MasterMind test_game(test_sol, game.n_coups, game.n_pins, game.n_coups_max);
 
     for (int c=0; c < game.n_coups; c++) {
@@ -146,11 +213,10 @@ bool check_sol(const MasterMind &game, const int *test_sol) {
 }
 
 int solve(MasterMind &game) {
-    auto next_coup = new int[game.n_pins] {0};
-
+    auto next_coup = make_unique<int[]>(game.n_pins);
+    initialize(next_coup, game.n_pins);
     while (!game.play(next_coup)) {
-        delete[] next_coup;
-        next_coup = new int[game.n_pins]{0};
+        initialize(next_coup, game.n_pins);
         while (!check_sol(game, next_coup)) {
             next_alphanum(next_coup, game.n_couleurs, game.n_pins);
         }
@@ -169,24 +235,21 @@ float moyenne(const int *tab, int t){
 
 int main() {
     srand(time(NULL));
-    int sol[] = {7, 7, 7, 7};
-    int solt[] = {3, 2, 1, 2};
-    int coupt[] = {2, 2, 2, 1};
-    MasterMind gt(solt, 8, 4, 25);
-    gt.play(coupt, true);
-    cout << gt << endl;
+    unique_ptr<int[]> sol;
+    sol.reset(new int[4]{7, 7, 7, 7});
+    //int solt[4] = {7, 7, 7, 7};
+    //initialize(sol, 4, solt);;
     MasterMind g(sol, 8, 4, 25);
     int n;
-    cout << in_array(1, sol, 4) << endl;
     n = solve(g);
     cout << g;
     std::cout << n << std::endl;
 
-    int *val = new int[2000];
-    for (int i=0; i<2000; i++){
+    int *val = new int[N];
+    for (int i=0; i<N; i++){
         g = MasterMind(8, 4, 25);
         val[i] = solve(g);
     }
-    cout << "\n" << moyenne(val, 2000) << endl;
+    cout << "\n" << moyenne(val, N) << endl;
     return 0;
 }
